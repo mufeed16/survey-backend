851b05ccc83ee64db46a9001814a2f3f1bdd88c0
mufeed <mufeeddaralmanar123@gmail.com>
2025-03-03 13:59:17 +0530

    backend updated

--- models/survey.js
+++ models/survey.js
@@ -5,6 +5,11 @@ const surveySchema = new mongoose.Schema({
     gender: { type: String, required: true, enum: ["Male", "Female", "Other"] },
     nationality: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    countryCode: { 
+        type: String, 
+        required: true, 
+        enum: ["+91", "+1", "+44", "+61", "+81", "+49", "+33", "+39", "+86", "+7", "+34", "+55", "+27", "+82", "+964", "+971", "+92", "+20", "+60", "+66"] 
+    }, 
     phoneNumber: { type: String, required: true },
     address: { type: String, required: true },
     message: { type: String, required: true },
